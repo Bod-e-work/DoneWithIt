1db5c36278cc025c0b35df06a84db9fcc7d3d007
Shawn Noel <drrl@gmail.com>
2023-02-11 01:22:45 -0500

    started navigation

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React, {useEffect, useState} from 'react';
 import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
 import { WebView } from 'react-native-webview';
-
 import { StyleSheet, Text, View, SafeAreaView , Platform, Dimensions, TextInput, Button, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import WelcomeScreen from './App/Screens/WelcomeScreen';
 import ViewImageScreen from './App/Screens/ViewImageScreen';
@@ -14,8 +16,8 @@ import ListingsScreen from './App/Screens/ListingsScreen';
 import AccountScreen from './App/Screens/AccountScreen';
 import Icons from './App/components/Icons';
 import Screen from './App/components/Screen';
-import AppTextInput from './App/components/AppTextInput';
-import AppPicker from './App/components/AppPicker';
+import AppTextInput from './App/components/TextInput';
+import AppPicker from './App/components/Picker';
 import LoginScreen from './App/Screens/LoginScreen';
 import AppButton from './App/components/AppButton';
 import { AppForm, AppFormField } from './App/components/forms';
@@ -23,14 +25,53 @@ import RegisterScreen from './App/Screens/RegisterScreen';
 import ListingEditScreen from './App/Screens/ListingEditScreen';
 import Icon from './App/components/Icons';
 import PickerItem from './App/components/PickerItem';
+import AuthNavigator from './App/navigation/AuthNavigator';
+
+
+// const Tweets = ({ navigation }) => (
+//   <Screen>
+//     <Text>Tweets</Text>
+//     <Button 
+//       title="View Tweet" 
+//       onPress={() => navigation.navigate("TweetDetails")} />
+//   </Screen>
+// )
+
+// const TweetDetails = () => (
+//   <Screen>
+//     <Text>TweetDetails</Text>
+//   </Screen>
+// )
+
+// const Stack = createStackNavigator();
+// const StackNavigator = () => {
+//   <Stack.Navigator>
+//     <Stack.Screen name="Tweets" component={Tweets} />
+//     <Stack.Screen 
+//     name="TweetDetails" 
+//     component={Tweets} 
+//     options={({ route }) => ({ tilte: route.params.id})}
+//     />
+//   </Stack.Navigator>
+// }
+
+// const Account = () => <Screen><Text>Account</Text></Screen>
+
+// TabNavigator = () => (
+//   <Tab.Navigator>
+//     <Tab.Screen name="Feed" component={Tweets} /> 
+//     <Tab.Screen name="Account" component={Account} /> 
+//   </Tab.Navigator>
+// )
+
 
 export default function App() {
-
-  (
-    <WelcomeScreen
-  />
+  return (
+  <></>
   );
-}
+};
+
+
 
 
 
